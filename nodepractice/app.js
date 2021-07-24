bcc0ff816b0c4660f7473c74f58c9492d72fadf5
kimsangwoo66 <56182236+kimsangwoo66@users.noreply.github.com>
2021-07-24 11:02:04 +0900

    front단 js파일 생성

--- nodepractice/app.js
+++ nodepractice/app.js
@@ -11,6 +11,7 @@ const home = require("./routes/home");
 app.set("views", "./views");
 app.set("view engine", "ejs"); //뷰엔진을 ejs 로 해석해 주기 위해 사용, ejs 는 html이라고 생각하고 사용
 
+app.use(express.static(`${__dirname}/public`));
 app.use("/", home); //use -> 미들웨어를 등록해주는 메서드
 
 module.exports = app;
