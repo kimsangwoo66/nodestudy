adfd165cafdeafb132082113f73acd1eb16c8f9c
kimsangwoo66 <56182236+kimsangwoo66@users.noreply.github.com>
2021-07-24 14:11:45 +0900

    dom의 defer 을 사용해서 html 객체 저장

--- nodepractice/public/js/home/login.js
+++ nodepractice/public/js/home/login.js
@@ -1,4 +1,20 @@
 "use strict";
 
-console.log("hello");
-console.log("bye");
+// 백단
+
+console.log("아니");
+const id = document.querySelector("#id"); //queryselector은 질의 선택자, id(선택자)로 불러온다.
+const password = document.querySelector("#password"); //queryselector 질의 선택자, password로 불러온다.
+const loginBtn = document.querySelector("button"); //queryselector 질의 선택자, button 으로 불러온다.
+
+loginBtn.addEventListener("click", login);
+
+function login() {
+  //   console.log(id.value);
+  const req = {
+    //받아올 파라미터를 오브젝트 형태로 받아옴
+    id: id.value,
+    password: password.value,
+  };
+  console.log(req);
+}
