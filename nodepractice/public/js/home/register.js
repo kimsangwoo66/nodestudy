b9c66fecedba4903f3a4b60c95b3266fa0853f83
kimsangwoo66 <56182236+kimsangwoo66@users.noreply.github.com>
2021-08-05 01:04:55 +0900

    nodejs: 회원가입 요청 구현

--- nodepractice/public/js/home/register.js
+++ nodepractice/public/js/home/register.js
@@ -2,25 +2,30 @@
 // html과 연결되어있는 javascript 파일
 // 프론트와 연결된 자바스크립트
 
-console.log("아니");
 const id = document.querySelector("#id"); //queryselector은 질의 선택자, id(선택자)로 불러온다.
+const name = document.querySelector("#name");
 const password = document.querySelector("#password"); //queryselector 질의 선택자, password로 불러온다.
-const loginBtn = document.querySelector("#button"); //queryselector 질의 선택자, button 으로 불러온다.
+const confirmPassword = document.querySelector("#confirm-password");
 
-loginBtn.addEventListener("click", login);
+const registerBtn = document.querySelector("#button"); //queryselector 질의 선택자, button 으로 불러온다.
+console.log("앙녕칭구"); //콘솔창에서 메시지가 나오게 되면 register.js 와 register.ejs가 연결확인
+registerBtn.addEventListener("click", register);
 
-function login() {
+function register() {
   //   console.log(id.value);
   const req = {
     //받아올 파라미터를 오브젝트 형태로 받아옴
     id: id.value,
+    name: name.value,
     password: password.value,
+    confirmPassword: confirmPassword.value,
   };
+
   // console.log(req);
   // console.log(JSON.stringify(req)); //json 객체는 문자열로 감싸져있음
 
   //응답한 데이터를 다시 받을려면 then()이라는 메소드를 사용해서 받아 올 수있다.
-  fetch("/login", {
+  fetch("/register", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -31,12 +36,12 @@ function login() {
     .then((res) => {
       if (res.success) {
         //로그인 성공시 루트 페이지로 이동
-        location.href = "/";
+        location.href = "/login";
       } else {
         alert(res.msg);
       }
     })
     .catch((err) => {
-      console.error(new Error("로그인 중 에러 발생"));
+      console.error(new Error("회원가입 중 에러 발생"));
     });
 }
