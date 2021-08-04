52ae1ddbdaa58a3c606bde16b19a42f7e0207430
kimsangwoo66 <56182236+kimsangwoo66@users.noreply.github.com>
2021-08-05 00:41:54 +0900

    nodejs: 회원가입 페이지 구현

--- nodepractice/routes/home/index.js
+++ nodepractice/routes/home/index.js
@@ -7,6 +7,7 @@ const ctrl = require("./home.ctrl");
 
 router.get("/", ctrl.output.hello); //api
 router.get("/login", ctrl.output.login); //api'
+router.get("/register", ctrl.output.register); //서버단에서 구현중
 router.post("/login", ctrl.process.login);
 // server에서 login data를 받는 api , 이것을 주석처리 하면 전달할 경로가 사라진다.
 
